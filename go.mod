d050408fab27c05d2361851bd2527330b31bd013
CodingForFunAndProfit <sfloxn@gmail.com>
2022-03-26 16:34:37 +0100

    example how the plugin system works

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/CodingForFunAndProfit/plugins
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"plugin"
+)
+
+type Greeter interface {
+	Greet()
+}
+
+func main() {
+
+	fmt.Println("Plguins loader started.")
+
+	// determine module to load
+	lang := "english"
+	if len(os.Args) == 2 {
+		lang = os.Args[1]
+	}
+	var mod string
+	switch lang {
+	case "english":
+		mod = "./plugin1/plugin1.so"
+	case "chinese":
+		mod = "./plugin2/plugin2.so"
+	default:
+		fmt.Println("don't speak that language")
+		os.Exit(1)
+	}
+
+	// load module
+	// 1. open the so file to load the symbols
+	plug, err := plugin.Open(mod)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// 2. look up a symbol (an exported function or variable)
+	// in this case, variable Greeter
+	symGreeter, err := plug.Lookup("Greeter")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// 3. Assert that loaded symbol is of a desired type
+	// in this case interface type Greeter (defined above)
+	var greeter Greeter
+	greeter, ok := symGreeter.(Greeter)
+	if !ok {
+		fmt.Println("unexpected type from module symbol")
+		os.Exit(1)
+	}
+
+	// 4. use the module
+	greeter.Greet()
+}
